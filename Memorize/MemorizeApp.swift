3ce6f46cd01ae64c67a6f8fd4c08300e792c1f85
Denis Ignatenko <106900482+DenisIgnatenko@users.noreply.github.com>
2023-10-03 18:11:31 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Денис Игнатенко on 03.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
