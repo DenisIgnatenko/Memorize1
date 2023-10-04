23c6cde9eb5061a5abfe34354552da5031c9142d
Denis Ignatenko <106900482+DenisIgnatenko@users.noreply.github.com>
2023-10-04 14:13:28 +0300

    Made array out of cards.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,11 +10,12 @@ import SwiftUI
 struct ContentView: View {
 
     var body: some View {
+        let emojis = ["🦾", "🤡", "👾", "🤯", "🤯"]
+        
         HStack {
-            CardView(isFaceUp: true)
-            CardView(isFaceUp: true)
-            CardView()
-            CardView(isFaceUp: true)
+            ForEach(emojis.indices, id: \.self) { index in
+                CardView(content: emojis[index])
+            }
         }
         .foregroundColor(.orange)
         .padding()
@@ -22,6 +23,7 @@ struct ContentView: View {
 }
 
 struct CardView: View {
+     let content: String
    @State var isFaceUp = false
     
     var body: some View {
@@ -30,7 +32,7 @@ struct CardView: View {
             if isFaceUp {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
-                Text("🦾").font(.largeTitle)
+                Text(content).font(.largeTitle)
             } else {
                 base.fill()
             }
